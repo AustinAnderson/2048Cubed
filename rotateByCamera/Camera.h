3c988baf7bf5af46126e1e3327c30034cc13c583
Austin Anderson <Austin.David.Anderson1@gmail.com>
2016-05-18 14:18:16 -0500

    trying to get it rotating right

---
+++ rotateByCamera/Camera.h
@@ -0,0 +1,55 @@
+
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "Algebra.h"
+
+class Camera {
+public:
+    Camera();
+    ~Camera();
+    void Orient(Point& eye, Point& focus, Vector& up);
+    void Orient(Point& eye, Vector& look, Vector& up);
+    void SetViewAngle (double viewAngle);
+    void SetNearPlane (double nearPlane);
+    void SetFarPlane (double farPlane);
+    void SetScreenSize (int screenWidth, int screenHeight);
+
+    Matrix GetProjectionMatrix();
+    Matrix GetModelViewMatrix();
+    Matrix GetOrthographicProjection();
+
+    void RotateV(double angle);
+    void RotateU(double angle);
+    void RotateUAround(double angle,const Point& center,double distance);
+    void RotateW(double angle);
+    void Rotate(Point p, Vector axis, double degree);
+    void RotateAround(Point p, Vector axis, double angle,const Point& center,double distance);
+    void Translate(const Vector &v);
+
+    Vector getV(){
+        return v;
+    }
+
+    Point GetEyePoint();
+    Vector GetLookVector();
+    Vector GetUpVector();
+    double GetViewAngle();
+    double GetNearPlane();
+    double GetFarPlane();
+    int GetScreenWidth();
+    int GetScreenHeight();
+
+    double GetFilmPlanDepth();
+    double GetScreenWidthRatio();
+    
+    
+private:
+    Point eye;
+    Vector look, up, u, v, w;
+    double view, Near, Far;
+    int width, height;
+};
+#endif
+
---
+++ rotateByGL/main.cpp
@@ -0,0 +1,393 @@
+//
+//  main.cpp
+//  GraphicsFinal
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glui.h>
+
+#include "Camera.h"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+const float CAMERA_MOVE_INC = 0.2;
+const float CAMERA_ZOOM_FACTOR = 1.5;
+const float CAMERA_ROTATE_FACTOR = 2;
+
+Camera* camera = new Camera();
+
+
+bool showGrid = true;
+bool debugMode = true;
+bool wireframe = false;
+
+bool lastRotateOnX = true;
+
+int	 camRotU = 0;
+int	 camRotV = 0;
+int	 camRotW = 0;
+int  viewAngle = 45;
+float eyeX = 0;
+float eyeY = 0.5;
+float eyeZ = 5;
+float lookX = 0;
+float lookY = 0;
+float lookZ = -1;
+Vector rotation(0,0,0);
+Vector lookV(lookX, lookY, lookZ);
+Vector upV(0, 1, 0);
+Point eyeP(eyeX, eyeY, eyeZ);
+float clipNear = 0.001;
+float clipFar = 30;
+std::ostream& operator<<(std::ostream& os,Point& p){
+    os<<"{"<<p[0]<<","<<p[1]<<","<<p[2]<<"}";
+    return os;
+}
+
+int main_window;
+
+void myGlutIdle(void);
+void myGlutDisplay(void);
+void onExit(void);
+void draw_grid();
+void myGlutReshape(int x, int y);
+void myKeyboardFunc(unsigned char key, int x, int y);
+void myKeyboardSpecFunc(int key, int x, int y);
+void displayText( float x, float y, int r, int g, int b, const char *string );
+
+Vector getFace(const Vector& lookV,const Vector& x=Vector(1,0,0),const Vector& y=Vector(0,1,0),const Vector& z=Vector(0,0,1)){
+    double dotx=dot(lookV,x);
+    double doty=dot(lookV,y);
+    double dotz=dot(lookV,z);
+    int maxNdx=0;
+    Vector toReturn(dotx,doty,dotz);
+    Vector abs(dotx,doty,dotz);
+    for(int i=0;i<3;i++){
+        if(abs[i]<0) abs[i]=-abs[i];
+        if(abs[maxNdx]<abs[i])maxNdx=i;
+    }
+    for(int i=0;i<3;i++){
+        if(i!=maxNdx) toReturn[i]=0;
+        else toReturn[i]/=abs[i];
+    }
+    //toReturn.negate();
+    return toReturn;
+}
+
+
+void displayText( float x, float y, int r, int g, int b, const char *string ) {
+    int j = strlen( string );
+    
+    glColor3f( r, g, b );
+    glRasterPos2f( x, y );
+    for( int i = 0; i < j; i++ ) {
+        glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, string[i] );
+    }
+}
+
+void myGlutIdle(void){
+    if(glutGetWindow() != main_window) glutSetWindow(main_window);
+    glutPostRedisplay();
+}
+void myGlutDisplay(void){
+    
+    if(wireframe){
+        glDisable(GL_POLYGON_OFFSET_FILL);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }else{
+        glEnable(GL_POLYGON_OFFSET_FILL);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    
+    glClearColor(.9f, .9f, .9f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    camera->SetViewAngle(viewAngle);
+    camera->SetNearPlane(clipNear);
+    camera->SetFarPlane(clipFar);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    Matrix projection = camera->GetProjectionMatrix();
+    glMultMatrixd(projection.unpack());
+    
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    camera->Orient(eyeP, lookV, upV);
+    glRotatef(rotation[0],1,0,0);
+    camera->RotateV(camRotV);
+    camera->RotateU(rotation[1]);
+    camera->RotateW(camRotW);
+    Matrix modelView = camera->GetModelViewMatrix();
+    glMultMatrixd(modelView.unpack());
+    
+    
+    //camera->RotateV(-camRotV);
+    //camera->RotateU(-camRotU);
+    //camera->RotateW(-camRotW);
+    
+    if(debugMode){
+        //TODO: fix text display
+        //displayText( 50,50, 255,0,0, "debug" );
+        draw_grid();
+        //drawing the axes
+        glDisable(GL_LIGHTING);
+        glLineWidth(4);
+        glBegin(GL_LINES);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex3f(0, 0, 0); glVertex3f(1.0, 0, 0);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex3f(0, 0, 0); glVertex3f(0.0, 1.0, 0);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex3f(0, 0, 0); glVertex3f(0, 0, 1.0);
+        glEnd();
+        glLineWidth(1);
+        glEnable(GL_LIGHTING);
+    }
+    int ndxs[3];
+    for(ndxs[0]=0;ndxs[0]<4;ndxs[0]++){
+        for(ndxs[1]=0;ndxs[1]<4;ndxs[1]++){
+            for(ndxs[2]=0;ndxs[2]<4;ndxs[2]++){
+                glPushMatrix();
+                for(int i=0;i<3;i++){
+                    if(ndxs[i]==3){
+                        if(0==i&&2==ndxs[1]&&2==ndxs[2]){
+                            glColor3f(1,0,0);
+                        }
+                        if(1==i&&2==ndxs[0]&&2==ndxs[2]){
+                            glColor3f(0,1,0);
+                        }
+                        if(2==i&&2==ndxs[0]&&2==ndxs[1]){
+                            glColor3f(0,1,1);
+                        }
+                    }
+                    else if(ndxs[i]==0){
+                        if(0==i&&2==ndxs[1]&&2==ndxs[2]){
+                            glColor3f(1,0,1);
+                        }
+                        if(1==i&&2==ndxs[0]&&2==ndxs[2]){
+                            glColor3f(1,1,0);
+                        }
+                        if(2==i&&2==ndxs[0]&&2==ndxs[1]){
+                            glColor3f(1,1,1);
+                        }
+                    }
+                }
+                glTranslatef(ndxs[0]*.25-.375,ndxs[1]*.25-.375,ndxs[2]*.25-.375);
+                glutSolidCube(.125);
+                glColor3f(0,0,1);
+                glPopMatrix();
+            }
+        }
+    }
+    
+    glColor3f(0,0,0);
+    glutSolidCube(20);
+    glutSwapBuffers();
+    glutPostRedisplay();
+
+}
+void increment(int axis,int value){
+    int v=int(rotation[axis]+value)%360;
+    if(v<0) v+=360;
+    rotation[axis]=v;
+}
+void myKeyboardSpecFunc(int key, int x, int y){
+    int inc=1;
+    if(key==GLUT_KEY_UP){
+        increment(0,inc);
+        if(!lastRotateOnX) lastRotateOnX=true;
+    }
+    if(key==GLUT_KEY_DOWN){
+        increment(0,-inc);
+        if(!lastRotateOnX) lastRotateOnX=true;
+    }
+
+    int otherAxis=1;
+    //if((rotation[0]<45||rotation[0]>315||(rotation[0]>135&&rotation[0]<225))){
+        //otherAxis=1;
+    //}
+    //if(otherAxis==2&&((rotation[1]>45&&rotation[1]<135)||(rotation[1]>225&&rotation[1]<315))){
+        //printf("was rotating on 2 now back on 1  ");
+        //otherAxis=1;
+    //}
+    //else{
+        //printf("                                 ");
+    //}
+    if(key==GLUT_KEY_LEFT){
+        increment(otherAxis,inc);
+        if(lastRotateOnX) lastRotateOnX=false;
+    }
+    if(key==GLUT_KEY_RIGHT){
+        increment(otherAxis,-inc);
+        if(lastRotateOnX) lastRotateOnX=false;
+    }
+    printf("{% 3d,% 3d,% 3d}",int(rotation[0]),int(rotation[1]),int(rotation[2]));
+    printf(" rotating on axis %d",otherAxis);
+    printf("\n");
+    
+    glutPostRedisplay();
+}
+
+void myKeyboardFunc(unsigned char key, int x, int y){
+    //std::cout << "key: " << key << "\nx: " << x << "\ny: " << y << std::endl;
+    
+    
+    /*
+     w - shift camera up
+     s - shift camera down
+     d - shift camera right
+     a - shift camera left
+     r - shift camera forward
+     f - shift camera back
+     + - zoom in
+     - - zoom out
+     */
+    switch(key){
+        case '0': debugMode = (debugMode)? false:true;
+            break;
+
+        case 'i': camRotU+=CAMERA_ROTATE_FACTOR;
+            break;
+            
+        case 'k': camRotU-=CAMERA_ROTATE_FACTOR;
+            break;
+            
+        case 'j': camRotV+=CAMERA_ROTATE_FACTOR;
+            break;
+            
+        case 'l': camRotV-=CAMERA_ROTATE_FACTOR;
+            break;
+            
+        case 'w': eyeP[1] += CAMERA_MOVE_INC;
+            break;
+            
+        case 's': eyeP[1] -= CAMERA_MOVE_INC;
+            break;
+            
+        case 'a': eyeP[0] -= CAMERA_MOVE_INC;
+            break;
+            
+        case 'd': eyeP[0] += CAMERA_MOVE_INC;
+            break;
+            
+        case 'r': eyeP[2] -= CAMERA_MOVE_INC;
+            break;
+            
+        case 'f': eyeP[2] += CAMERA_MOVE_INC;
+            break;
+            
+        case '+': eyeP[0] /= CAMERA_ZOOM_FACTOR;
+                  eyeP[1] /= CAMERA_ZOOM_FACTOR;
+                  eyeP[2] /= CAMERA_ZOOM_FACTOR;
+            break;
+            
+        case '-': eyeP[0] *= CAMERA_ZOOM_FACTOR;
+                  eyeP[1] *= CAMERA_ZOOM_FACTOR;
+                  eyeP[2] *= CAMERA_ZOOM_FACTOR;
+            break;
+            
+        case ' ': camRotV=0;
+                  camRotU=0;
+                  camRotW=0;
+            break;
+            
+        case 'q': wireframe = (wireframe)? false:true;
+            break;
+    }
+    
+    glutPostRedisplay();
+}
+
+void onExit(void){
+    if(camera){
+        delete camera;
+    }
+    
+}
+
+void myGlutReshape(int x, int y){
+    float xy_aspect;
+    
+    xy_aspect = (float)x / (float)y;
+    glViewport(0, 0, x, y);
+    
+    camera->SetScreenSize(x, y);
+    
+    glutPostRedisplay();
+}
+
+void draw_grid(){
+    /*
+    int grid_size = 20;
+    glDisable(GL_LIGHTING);
+    glPushMatrix();
+    glTranslatef(-grid_size/2,0,-grid_size/2);
+    glColor3f( .2, .2, .2 );
+    for(float i = 0 ; i < grid_size; i+=.2){
+        glBegin(GL_LINES);
+        glVertex3f( 0.0, 0.0, i );  glVertex3f( grid_size, 0.0, i );
+        glVertex3f( i, 0.0, 0.0 );  glVertex3f( i, 0.0, grid_size );
+        glEnd();
+    }
+    
+    glBegin(GL_LINES);
+    glVertex3f( 0.0, 0.0, grid_size );  glVertex3f( grid_size, 0.0, grid_size );
+    glVertex3f( grid_size, 0.0, 0.0 );  glVertex3f( grid_size, 0.0, grid_size );
+    glEnd();
+    glPopMatrix();
+    glEnable( GL_LIGHTING );
+    */
+    
+}
+
+int main(int argc, char* argv[]){
+    atexit(onExit);
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 600);
+    
+    main_window = glutCreateWindow("Graphics Final");
+    glutDisplayFunc(myGlutDisplay);
+    glutReshapeFunc(myGlutReshape);
+    glutKeyboardFunc(myKeyboardFunc);
+    glutSpecialFunc(myKeyboardSpecFunc);
+
+    
+    glClearColor (0.48, 0.48, 0.48, 0.0);
+    glShadeModel (GL_SMOOTH);
+    
+    GLfloat light_pos0[] = {0.0f, 0.0f, 1.0f, 0.0f};
+    GLfloat diffuse[] = {0.5f, 0.5f, 0.5f, 0.0f};
+    GLfloat ambient[] = {0.05f, 0.05f, 0.05f, 0.0f};
+    
+    glLightfv (GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv (GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv (GL_LIGHT0, GL_POSITION, light_pos0);
+    
+    glColorMaterial (GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable (GL_COLOR_MATERIAL);
+    
+    glEnable(GL_LIGHTING);
+    glEnable (GL_LIGHT0);
+    glEnable (GL_DEPTH_TEST);
+
+    glPolygonOffset(1, 1);
+    
+    
+    srand (time(NULL));
+    
+    
+    glutMainLoop();
+    
+    return EXIT_SUCCESS;
+    
+}
